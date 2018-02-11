0debe5a916ff5586bdf99f4e9227b34062c44ab1
jorge silva <jsilvajr1@yahoo.com>
2018-02-11 14:40:28 -0500

    error update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,31 +1,19 @@
 class PlacesController < ApplicationController
-  
-  
   def index
     @places = Place.all 
-   @places = Place.paginate(:page => params[:page], :per_page => 5)
-    end
-end
-
- 
-
-
-
-def new
-  @place = Places.new
+    @places = Place.paginate(:page => params[:page], :per_page => 5)
   end
-def create
-end
 
-private
-
-def create
-    Place.create(place_params)
-  redirect_to root_path
+  def new
+    @place = Places.new
+  end
+  
+  def create
   end
 
   private
 
   def place_params
-    params.require(:place).permit(:name, :description, :address)
+  params.require(:place).permit(:name, :description, :address)
   end
+end

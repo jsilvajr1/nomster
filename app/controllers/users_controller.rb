5aa5a14d5a262f3995a2f4a7fe61d366c4037ecd
jorge silva <jsilvajr1@yahoo.com>
2018-02-21 20:48:32 -0500

    users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    end
+end
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
